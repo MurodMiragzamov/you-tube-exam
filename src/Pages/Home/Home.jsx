8c37fb390b95fdb784612eb238ffc93dc75447c0
Hugo <blackfoxmmm@gmail.com>
2021-09-19 14:51:59 +0500

    ihgib

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,27 @@
 import React from "react";
-
+import "./Home.scss";
 
 function Home() {
-  return <>home</>;
+  const [data, setData] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((response) => response.json())
+      .then((data) => setData(data.slice(0, 5)));
+  }, []);
+  return (
+    <>
+      {/* <ul>
+        {data &&
+          data.map((row) => (
+            <li key={row.id}>
+              <h2>{row.title}</h2>
+              <img src={row.url} alt="icon" />
+            </li>
+          ))}
+      </ul> */}
+    </>
+  );
 }
 
 export default Home;
