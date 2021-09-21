333714e4ab48944ab62cd8c9f743543b5367b57a
Hugo <blackfoxmmm@gmail.com>
2021-09-21 16:44:59 +0500

    ferfe

--- src/Pages/Videos/Videos.jsx
+++ src/Pages/Videos/Videos.jsx
@@ -34,7 +34,86 @@ function Videos() {
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen
               ></iframe>
+              <h2 className="mesto"></h2>
+              <h2 className="video-header">Dude You Re Getting A Telescope</h2>
+
+              <div className="video_div">
+                <p className="video-p">123k views</p>
+
+                <ul className="popular-list">
+                  <li className="popular-item">
+                    <img
+                      src={likeruka}
+                      alt="like"
+                      width={100}
+                      height={100}
+                      className="popular-like"
+                    />
+                    <button className="popular-btn">123k</button>
+                  </li>
+                  <li className="popular-item">
+                    <img
+                      src={dislike}
+                      alt="dislike"
+                      width={100}
+                      height={100}
+                      className="popular-dislike"
+                    />
+                    <button className="popular-btn">435k</button>
+                  </li>
+                  <li className="popular-item">
+                    <img
+                      src={share}
+                      alt="share"
+                      width={100}
+                      height={100}
+                      className="popular-share"
+                    />
+                    <button className="popular-btn">Share</button>
+                  </li>
+                  <li className="popular-item nonee">
+                    <img
+                      src={more}
+                      alt="more"
+                      width={100}
+                      height={100}
+                      className="popular-more"
+                    />
+                  </li>
+                </ul>
+                <span className="span-line"></span>
+              </div>
+
+              <div className="video_food">
+                <span className="food-span">
+                  <img
+                    src={apelsin}
+                    alt="more"
+                    width={100}
+                    height={100}
+                    className="food-apelsin"
+                  />
+
+                  <h4 className="food-header">Food & Drink</h4>
+
+                  <p className="food-p">Published on 14 Jun 2019</p>
+                </span>
+
+                <button className="food_btn">Subscribe 2.3m</button>
+              </div>
+
+              <p className="video_food_p">
+                A successful marketing plan relies heavily on the pulling-power
+                of advertising copy. Writing result-oriented ad copy is
+                difficult, as it must appeal to, entice, and convince consumers
+                to take action. There is no magic formula to write perfect ad
+                copy; it is based on a number of factors, including ad
+                placement, demographic, even the consumer’s mood when they see
+                your ad.
+              </p>
+              <button className="video_show">Show more</button>
             </li>
+
             <li className="video-item-videos">
               <ul className="video_li_ul">
                 <div className="auto-div">
@@ -72,8 +151,8 @@ function Videos() {
                       </NavLink>
                       <h4 className="video_li_header">{row.title}</h4>
                       <span className="span-p">
-                        <p className="video_li_p ">13k views · 1 months ago</p>
-                        <p className="video_li_p ">Food & Drink</p>
+                        <p className="video_li_p ">123k views</p>
+                        <p className="video_li_p ">Dollie Blair</p>
                       </span>
                     </li>
                   ))}
